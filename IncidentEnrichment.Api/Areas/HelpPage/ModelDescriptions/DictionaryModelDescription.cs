18fd494d90726e8cac71bb48eb8f563339658fd3
Aliyar Taravati <ataravati@gmail.com>
2018-11-04 17:15:23 -0600

    Deleted the IncidentEnrichment.Api project...

--- IncidentEnrichment.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace IncidentEnrichment.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- IncidentEnrichment.Api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace IncidentEnrichment.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
