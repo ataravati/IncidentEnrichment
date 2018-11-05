a6e0aaf555c499ba2c9a7e29d47e628970e733fd
Aliyar Taravati <ataravati@gmail.com>
2018-11-04 19:19:08 -0600

    Added the angularjs main module, and the map controller with Google Maps API...

---
+++ IncidentEnrichment/app/MapCtrl.js
@@ -0,0 +1,18 @@
+﻿﻿(function () {
+    'use strict';
+
+    var module = angular.module("main");
+    module.controller('MapCtrl',
+        ["$scope", "$rootScope", "$compile",
+            function ($scope, $rootScope, $compile) {
+                function initialize() {
+                    $scope.map = new google.maps.Map(document.getElementById('map'), {
+                        zoom: 10,
+                        center: { lat: 37.5407, lng: -77.4360 } // Richmond, VA
+                    });
+                }
+
+                google.maps.event.addDomListener(window, 'load', initialize);
+            }
+        ]);
+}());
---
+++ IncidentEnrichment/app/main.js
@@ -0,0 +1,9 @@
+﻿﻿(function () {
+    'use strict';
+
+     var app = angular.module("main", []);
+
+    app.constant('appSettings', {
+        apiServiceBaseUri: 'http://localhost:52041/'
+    });
+}()); 
